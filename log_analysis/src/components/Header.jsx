ac64d95ef95088093b8d0a75e4c2920307d3a629
Abhishek Swaroop <abhi.swrp4321@gmail.com>
2024-12-05 02:45:23 +0530

    Modifications

--- log_analysis/src/components/Header.jsx
+++ log_analysis/src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
   return (
     <header className="header">
       <div className="logo">
-        <img src="assets/Swaroop's_Logo.svg" alt="Logo" />
+        <img src="assets/Swaroop's_Logo.png" alt="Logo" />
       </div>
       <nav>
         <ul>
